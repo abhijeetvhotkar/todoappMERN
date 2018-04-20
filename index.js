66770418b7099135fd9fba048cc469e7d91beb13
Abhijeet Vhotkar <abhijeetvhotkar@gmail.com>
2018-04-20 03:11:47 -0400

    Added Schema

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'),
   app = express();
 
 app.get('/', function(req, res){
-  res.send("Hi there from express");
+  res.json({message: "Hi there from express"});
 })
 
 app.listen(3000, function(){
